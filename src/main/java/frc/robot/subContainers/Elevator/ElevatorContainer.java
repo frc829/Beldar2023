a3cf556d31377a258874cd88eb563561e13ad64d
narmstro2020 <narmstro@warren.k12.in.us>
2023-03-06 12:48:41 -0500

    Prepped

--- src/main/java/frc/robot/subContainers/Elevator/ElevatorContainer.java
+++ src/main/java/frc/robot/subContainers/Elevator/ElevatorContainer.java
@@ -2,14 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot.subContainers.Elevator;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+/** Add your docs here. */
+public class ElevatorContainer {
 
-public class Arm extends SubsystemBase {
-
-
-
-
+    public ElevatorContainer(){
+        
+    }
 
 }
